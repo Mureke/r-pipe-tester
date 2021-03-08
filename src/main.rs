5b5e7d468919da0092e6220910af87c5f8985dd3
Markus Suomalainen <markus.suomalainen@gmail.com>
2021-03-08 14:56:49 +0200

    Working pipe server

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::fs;
 
 use interprocess::local_socket::{LocalSocketListener, LocalSocketStream};
 use shutdown_hooks::add_shutdown_hook;
+
 fn handle_error(conn: io::Result<LocalSocketStream>) -> Option<LocalSocketStream> {
     match conn {
         Ok(val) => Some(val),
@@ -16,7 +17,7 @@ fn handle_error(conn: io::Result<LocalSocketStream>) -> Option<LocalSocketStream
 }
 
 
-extern "C" fn shutdown_hook(){
+extern "C" fn shutdown_hook() {
     fs::remove_file("/tmp/test");
 }
 
@@ -25,34 +26,39 @@ fn main() {
     add_shutdown_hook(shutdown_hook);
 
     // Pipe server
-    thread::spawn(|| {
-        let listener = LocalSocketListener::bind("/tmp/test").unwrap();
-        for mut conn in listener.incoming().filter_map(handle_error) {
-            let mut contents = "Server response";
-            conn.write_all(contents.as_bytes());
+
+    let _write_thread = thread::spawn(|| {
+        thread::sleep(Duration::from_secs(3));
+
+        let mut i: u32 = 0;
+        loop {
+            // WRITE
+            let mut stream = LocalSocketStream::connect("/tmp/test").unwrap();
+            stream.set_nonblocking(true);
+            let mut contents = i.to_string();
+            stream.write_all(contents.as_bytes());
+            i = i + 1;
+            thread::sleep(Duration::from_secs(3));
 
             let mut buffer = String::new();
-            conn.read_to_string(&mut buffer);
-            println!("Client request: {}", buffer);
-
+            stream.read_to_string(&mut buffer).unwrap();
+            println!("Server answered: {}", buffer);
+            thread::sleep(Duration::from_secs(3));
         }
     });
-    thread::sleep(Duration::from_secs(3));
 
-    let mut i: u32 = 0;
-    loop {
-        // WRITE
-        let mut stream = LocalSocketStream::connect("/tmp/test").unwrap();
-        stream.set_nonblocking(true);
-        let mut contents = i.to_string();
-        stream.write_all(contents.as_bytes());
-        i = i+1;
+    let listener = LocalSocketListener::bind("/tmp/test").unwrap();
+    for mut conn in listener.incoming().filter_map(handle_error) {
+        conn.set_nonblocking(true);
 
-        thread::sleep(Duration::from_secs(8));
-
-        // Read from pipe
         let mut buffer = String::new();
-        stream.read_to_string(&mut buffer);
-        println!("Server answered: {}", buffer);
+        conn.read_to_string(&mut buffer);
+        println!("Client request: {}", buffer);
+
+        let mut contents = format!("ẞerver Response {}", buffer);
+        conn.write_all(contents.as_bytes());
     }
+
+
+
 }
